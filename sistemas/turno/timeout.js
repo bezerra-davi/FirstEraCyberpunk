36539460f2bba047fd8e76b3332f5daa395c0846
ÁlvaroDiaz <corujade3olhos@gmail.com>
2026-05-22 07:43:31 -0300

    Create timeout.js

---
+++ sistemas/turno/timeout.js
@@ -0,0 +1,8 @@
+function timeout(tempo, texto1, texto2){
+    console.log(texto1)
+    setTimeout(() => {
+        console.log(texto2)// insira dialogos, tempo de respostas e etc...
+    }, tempo// aqui defina o tempo que vai demorar para aparecer
+    )
+}
+
