4e02dd6088a40df9c8f3e740b7475355b0d73dc4
ÁlvaroDiaz <corujade3olhos@gmail.com>
2026-05-22 18:23:30 -0300

    Update primeiraRota.js

--- sistemas/rotas/primeiraRota.js
+++ sistemas/rotas/primeiraRota.js
@@ -3,8 +3,10 @@ export function escolherCaminho1(caminho, opcao1, opcao2, opcao3){
     switch(caminho){
         case 1:
              opcao1;
+             break
             case 2:
-                 opcao2;
+                opcao2;
+                break
                 case 3:
                     return opcao3;
     
