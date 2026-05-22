e24acc8df7df9e353e684352aa3df33e12efe63e
marcovasconce <marcoaluno@edu.unifor.br>
2026-05-22 18:13:25 -0300

    implementacao do dano aleatorio e critico.js

--- sistemas/combate/acoes.js
+++ sistemas/combate/acoes.js
@@ -1,13 +1,16 @@
 import * as Status from './personagem/status.js';   // isso aqui e pra pegar as funcoes la do status.js
 import * as Habilidades from './habilidades/habilidades.js';
 import * as Itens from './itens/itens.js';
+import * as TipoDeDano from './combate/tipodeDano.js';
 
-export function atacar(atacante, alvo){
-    Status.tomarDano(alvo, atacante.ataque);
+export function atacar(atacante, alvo) {
+    let dano = TipoDeDano.danoAleatorio(atacante.ataque);
+    dano = TipoDeDano.calcularCritico(dano);
+    Status.tomarDano(alvo, dano);
 }
 
 export function defender(danoRecebido){
-    let danoDefendido = Math.floor(Math.random() * danoRecebido) + 1
+    let danoDefendido = Math.floor(Math.random() * 30) + 1
     let defesa = danoRecebido - danoDefendido
     if(defesa < 0){
     return defesa = 0
