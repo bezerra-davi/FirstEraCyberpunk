60c52cf536d9b61bc47e7017076e3c19d5e9376d
marcovasconce <marcoaluno@edu.unifor.br>
2026-05-22 21:51:07 -0300

    tentando explicar o readline e o async / wait

--- input.js
+++ input.js
@@ -1,18 +1,23 @@
-import * as readline from 'readline';
+// a gente precisava desse readline pq aparentemente o prompt do javascript não funciona no node.js pra executar no terminal
+// ele basicamente consegue ler oq a pessoa digita no terminal
 
-const rl = readline.createInterface({ 
-  input: process.stdin, 
-  output: process.stdout 
+const rl = readline.createInterface({  // esse createInterface conecta o readline com o terminal do computador
+  input: process.stdin,  // process.stdin e como se fosse a "entrada" do terminal, o teclado da pessoa
+  output: process.stdout // process.stdout e a "saída" do terminal, o que aparece na tela
 });
 
+// criei essa função aqui pra substituir o prompt, ela para o código e espera o usuário digitar algo
+// esse await na frente dela faz o código esperar a resposta antes de continuar
+
 export function lerInput(texto) {
-  return new Promise(resolve => {
-    rl.question(texto, (resposta) => {
-      resolve(resposta);
+  return new Promise(function(resolve) {
+    rl.question(texto, function(resposta) {
+      resolve(resposta); // quando a pessoa aperta Enter, ele envia a resposta de volta
     });
   });
 }
 
+// fecha o terminal quando o jogo termina
 export function fecharInput() {
   rl.close();
 }
