8c54953121da6f2c8a91cfd713f3af90e7290134
ÁlvaroDiaz <corujade3olhos@gmail.com>
2026-06-03 08:16:15 -0300

    Update floresta.js

--- historia/cenas/floresta.js
+++ historia/cenas/floresta.js
@@ -6,7 +6,7 @@ import { lerInput, printLento } from '../../input.js';
 export async function executarCenaFloresta(cena, personagem) {
     switch(cena) {
         case 1:
-        console.log(`Você se encontra em uma trilha no meio da floresta e fica sem saber o que fazer.`);
+        await printLento(`Você se encontra em uma trilha no meio da floresta e fica sem saber o que fazer.`);
         console.log(`1 - Ir para frente`);
         console.log(`2 - Ir para a direita`);
         console.log(`3 - Ir para a esquerda`);
@@ -21,19 +21,19 @@ export async function executarCenaFloresta(cena, personagem) {
             return 4;
         }
         case 2:
-        console.log(`Você encontrou um muro.`);
+        await printLento(`Você encontrou um muro.`);
         await lerInput(`[Enter para voltar]`);
         return 1;
         case 3:
-        console.log(`Você encontra um lago, nele tem alguns peixes, mas nada muito de interessante.`);
+        await printLento(`Você encontra um lago, nele tem alguns peixes, mas nada muito de interessante.`);
         await lerInput(`[Enter para voltar]`);
         return 1;
         case 4:
-        console.log(`Você encontra mais caminho e seguindo por mais um pouco você encontra uma clareira.`);
+        await printLento(`Você encontra mais caminho e seguindo por mais um pouco você encontra uma clareira.`);
         await lerInput(`[Enter para continuar]`);
-        console.log(`O lugar parece muito aconchegante, porém você nota que começa a ficar com fome.`);
+        await printLento(`O lugar parece muito aconchegante, porém você nota que começa a ficar com fome.`);
         await lerInput(`[Enter para continuar]`);
-        console.log(`De repente você encontra um coelho azul e você fica surpreso com a tamanha conveniência.`);
+        await printLento(`De repente você encontra um coelho azul e você fica surpreso com a tamanha conveniência.`);
         await lerInput(`[Enter para continuar]`);
         const coelho = Status.criarPersonagem(`Coelho Azul`, 30, 0, 8, 2, 20, false);
         await Turno.iniciarCombate(personagem, coelho);
