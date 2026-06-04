c29b20c97fa8791f929a5af8a00a2fc598126690
ÁlvaroDiaz <corujade3olhos@gmail.com>
2026-06-04 20:44:13 -0300

    Update dentroCabana.js

--- historia/cenas/dentroCabana.js
+++ historia/cenas/dentroCabana.js
@@ -110,7 +110,7 @@ export async function executarCenaDentroCabana(cena, personagem) {
             await lerInput(`[Enter para continuar]`);
             await printLento(`Ao abri-la você enxerga uma coisa`)
             await lerInput(`[Enter para continuar]`);
-            const itemCaixa = Itens.abrirBau(itens)
+            const itemCaixa = Itens.abrirBau(.Itens.itens)
             Status.pegarItem(personagem, itemCaixa)
             await printLento(`Você obteve um item: ${itemCaixa.nome}`)
             await lerInput(`[Enter para continuar]`);
