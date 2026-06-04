82bc5accd6b9f30f9409cef531f15b5388c8ead4
ÁlvaroDiaz <corujade3olhos@gmail.com>
2026-06-04 12:57:36 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ import { criarPersonagem } from './sistemas/personagem/status.js';
 import { iniciarJogo } from './historia/rotas.js';
 
 // ISSO E SO A DEMO PRA UM TESTE
-const player = criarPersonagem(`Heroi`, 100, 50, 30, 20, 15, true);
+const player = criarPersonagem(`Herói`, 100, 50, 30, 20, 15, true);
 await iniciarJogo(player);
