be39194a0f07c83f534ba032a5abea62a64197ec
ÁlvaroDiaz <corujade3olhos@gmail.com>
2026-06-04 21:48:13 -0300

    Update lutaFinal.js

--- historia/cenas/lutaFinal.js
+++ historia/cenas/lutaFinal.js
@@ -3,3 +3,7 @@ import * as Status from '../../sistemas/personagem/status.js';
 import * as Turno from '../../sistemas/turno/turno.js';
 import * as Itens from '../../sistemas/itens/itens.js';
 import * as Habilidades from '../../sistemas/habilidades/habilidades.js';
+
+export async function executarCenaLutaFinal(cena, personagem){
+  
+}
