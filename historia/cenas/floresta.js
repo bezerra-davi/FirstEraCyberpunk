1c61a48befb653296e7962a2b4c5bbf5d331cf9b
ÁlvaroDiaz <corujade3olhos@gmail.com>
2026-06-05 08:29:55 -0300

    Update floresta.js

--- historia/cenas/floresta.js
+++ historia/cenas/floresta.js
@@ -19,7 +19,7 @@ export async function executarCenaFloresta(cena, personagem) {
                 return 3;
             }
             else if (escolha === 3){
-                return 69;
+                return 4;
             }
             break;
 
