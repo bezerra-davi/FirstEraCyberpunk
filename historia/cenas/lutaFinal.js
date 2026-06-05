d81bfac0ca4187d6206d5933ed8490279f28efe8
ÁlvaroDiaz <corujade3olhos@gmail.com>
2026-06-04 21:43:03 -0300

    Update lutaFinal.js

--- historia/cenas/lutaFinal.js
+++ historia/cenas/lutaFinal.js
@@ -1,1 +1,5 @@
-ads
+import { lerInput, printLento, esperar } from '../../input.js';
+import * as Status from '../../sistemas/personagem/status.js';
+import * as Turno from '../../sistemas/turno/turno.js';
+import * as Itens from '../../sistemas/itens/itens.js';
+import * as Habilidades from '../../sistemas/habilidades/habilidades.js';
