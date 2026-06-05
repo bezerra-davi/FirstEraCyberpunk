673da8edc1f5340fb9ce7f30d4d0823572d5d8a1
ÁlvaroDiaz <corujade3olhos@gmail.com>
2026-06-05 08:07:14 -0300

    Update lutaFinal.js

--- historia/cenas/lutaFinal.js
+++ historia/cenas/lutaFinal.js
@@ -7,11 +7,20 @@ import * as Habilidades from '../../sistemas/habilidades/habilidades.js';
 export async function executarCenaLutaFinal(cena, personagem){
   switch(cena) {
     case 70: 
-    printLento(`${Status.personagem.nome} e Pygma se encaram, talvez, por uma ultima vez...`)
-    lerInput(`[Enter para continuar]`)
-    printLento(`Pygma começa a se contorcer. Seu corpo se retorce em uma transformação dolorosa, enquanto gritos de pura agonia ecoam por toda a masmorra... Quando tudo finalmente se acalma, Pygma já não é mais humano. Agora, transformado, ele é apenas um monstro que precisa ser derrotado.`)
-    lerInput(`[Enter para continuar]`)
+    await printLento(`${personagem.nome} e Pygma se encaram, talvez, por uma ultima vez...`)
+    await lerInput(`[Enter para continuar]`)
+    await printLento(`Pygma começa a se contorcer. Seu corpo se retorce em uma transformação dolorosa, enquanto gritos de pura agonia ecoam por toda a masmorra... Quando tudo finalmente se acalma, Pygma já não é mais humano. Agora, transformado, ele é apenas um monstro que precisa ser derrotado.`)
+    await lerInput(`[Enter para continuar]`)
     const pygma = Status.criarPersonagem(`pygma`, 40, 30, 6, 4, 15, false)
-    await Turno.iniciarCombate(personagem, inimigo)
+    await Turno.iniciarCombate(personagem, pygma)
+    await printLento(`Após uma longa batalha, Pygma se encontra jogado em meio às pedras frias da masmorra.`);
+    await lerInput(`[Enter para continuar]`);
+    await printLento(`Seus olhos estão quase sem vida, e seu corpo, destroçado.`);
+    await lerInput(`[Enter para continuar]`);
+    await printLento(`A batalha foi cruel. Um buscava a liberdade, e o outro, viver em uma eterna ilusão.`);
+    await lerInput(`[Enter para continuar]`);
+    await printLento(`Pygma fecha os olhos e morre no silêncio pós-batalha.`);
+    await lerInput(`[Enter para continuar]`);
+    return 71;
   }
 }
